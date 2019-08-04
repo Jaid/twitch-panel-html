13eb5b9c0a159c2b7d6350135331e7d67393b922
Jaid <jaid.jsx@gmail.com>
2019-08-04 07:28:42 +0200

    fix: Fixed `hasLink` determining
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/query.js
+++ src/query.js
@@ -3,7 +3,9 @@ import {parse} from "query-string"
 /**
  * @type {Object}
  */
-const query = parse(document.location.search)
+const query = parse(document.location.search, {
+  parseBooleans: true,
+})
 
 const defaults = {
   mode: "twitch",
