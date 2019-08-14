317838c4dfd2698067f3a8ad507fb00bc7d344cb
Jaid <jaid.jsx@gmail.com>
2019-08-14 03:53:21 +0200

    fix: Removed debugging code
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/query.js
+++ src/query.js
@@ -9,7 +9,4 @@ const defaults = {
   mode: "twitch",
 }
 
-query.content = "{iconcenter:lock/Nur für Mods}{iconcenter:lock/Nur für Subscriber und für Mods und für dies und für das}{bold:Beispiel}:{br:5}{chat:a\nb}"
-query.themeColor = "hsl(100, 100%, 50%)"
-
 export default Object.assign(defaults, query)
