becb45a6be9d360b04092564d7d754e16756735b
Jaid <jaid.jsx@gmail.com>
2019-08-15 09:09:01 +0200

    fix: Removed debugging code
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/query.js
+++ src/query.js
@@ -9,8 +9,4 @@ const defaults = {
   mode: "twitch",
 }
 
-query.hasLink = true
-query.icon = "github"
-query.title = "Uff, das hier ist ein ganz schön langer Text."
-
 export default Object.assign(defaults, query)
